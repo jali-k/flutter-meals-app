b1d8ddee22232c57c21871304222e6abcbf9473a
jali-k <e19210@eng.pdn.ac.lk>
2023-09-04 00:13:02 +0530

    Display each meal with meal item widget

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/Meal.dart';
 import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/category_item.dart';
 
@@ -8,7 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   void _selectCategory(BuildContext context, Category category) {
-    var filteredMeals = dummyMeals
+    List<Meal> filteredMeals = dummyMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     Navigator.of(context).push(
--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/Meal.dart';
 
+import '../widgets/meal_item.dart';
+
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
@@ -20,7 +22,12 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       bodyContent = Column(
         mainAxisSize: MainAxisSize.min,
-        children: [for (final meal in meals) Text(meal.title)],
+        children: [
+          for (final meal in meals)
+            MealItem(
+              meal: meal,
+            )
+        ],
       );
     }
 
