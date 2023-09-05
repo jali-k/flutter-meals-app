a1522ba82d1854d6ab8a12015022d300063ee4fd
jali-k <e19210@eng.pdn.ac.lk>
2023-09-05 18:59:55 +0530

    Set all the  filters neede

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -10,6 +10,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _setGlutienFree = false;
+  var _setLactoseFree = false;
+  var _setVegetarian = false;
+  var _setVegan = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -35,6 +38,72 @@ class _FiltersScreenState extends State<FiltersScreen> {
             ),
             subtitle: Text(
               "Filter the glutein free mels",
+              style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 12),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+          ),
+          SwitchListTile(
+            contentPadding: EdgeInsets.only(left: 20, right: 16),
+            value: _setLactoseFree,
+            onChanged: (isChecked) {
+              setState(() {
+                _setLactoseFree = isChecked;
+              });
+            },
+            title: Text(
+              "Is lactose free",
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 18),
+            ),
+            subtitle: Text(
+              "Filter the lactose free mels",
+              style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 12),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+          ),
+          SwitchListTile(
+            contentPadding: EdgeInsets.only(left: 20, right: 16),
+            value: _setVegetarian,
+            onChanged: (isChecked) {
+              setState(() {
+                _setVegetarian = isChecked;
+              });
+            },
+            title: Text(
+              "Vegetarian",
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 18),
+            ),
+            subtitle: Text(
+              "Filter the vegetarian mels",
+              style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 12),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+          ),
+          SwitchListTile(
+            contentPadding: EdgeInsets.only(left: 20, right: 16),
+            value: _setVegan,
+            onChanged: (isChecked) {
+              setState(() {
+                _setVegan = isChecked;
+              });
+            },
+            title: Text(
+              "Vegan",
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 18),
+            ),
+            subtitle: Text(
+              "Filter the vegan mels",
               style: Theme.of(context).textTheme.labelSmall!.copyWith(
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 12),
