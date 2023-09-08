f0f40db33dacf8ecc05fe6e59bce93500555d61f
jali-k <e19210@eng.pdn.ac.lk>
2023-09-09 00:06:27 +0530

    Create an AnimationController to create explicite animations

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,13 +5,34 @@ import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/category_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController
+      _animationController; // late tells that at the declaration this variable will not be having any value, but it will when it is being used.
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(microseconds: 300),
+      lowerBound: 0,
+      upperBound:
+          1, // These boudry values are default values you can change them
+    );
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    List<Meal> filteredMeals = availableMeals
+    List<Meal> filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     Navigator.of(context).push(
